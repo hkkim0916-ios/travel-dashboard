4f758676faba230ef46b4dfc14291956e1c99bb7
hkkim0916-ios <hkkim0916@gmail.com>
2026-07-02 14:51:48 +0900

    Remove unused CSS import from main.jsx
    Remove unused CSS import statement.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // CSS 파일이 없더라도 에러 방지를 위해 기본 연결해 둡니다.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
